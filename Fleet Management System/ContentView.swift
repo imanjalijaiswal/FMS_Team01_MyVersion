210e7a387455667c366482c576c6e2b21c933630
Aditya Mathur <166909570+adityam2003@users.noreply.github.com>
2025-03-17 16:40:52 +0530

    create fms project

---
+++ Fleet Management System/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fleet Management System
+//
+//  Created by Aditya Mathur on 17/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fleet Management System/Fleet_Management_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fleet_Management_SystemApp.swift
+//  Fleet Management System
+//
+//  Created by Aditya Mathur on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Fleet_Management_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
